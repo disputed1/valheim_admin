e6c4113b03a1c311e5b79e578cf1885596404186
J.Mac <jmcnab1990@gmail.com>
2021-02-08 12:08:31 -0500

    Create server_start.sh

---
+++ server_start.sh
@@ -0,0 +1,13 @@
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+# Tip: Make a local copy of this script to avoid it being overwritten by steam.
+# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+# NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
+
+valheimserver=`./valheim_server.x86_64 -name "****" -port 2456 -world "****" -password "****" -public 1 > /dev/null &`
+
+export LD_LIBRARY_PATH=$templdpath
+
+$valheimserver
