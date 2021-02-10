64e0a542fd79dfe55dcd51ef27d6770cb2640998
J.Mac <jmcnab1990@gmail.com>
2021-02-10 12:11:07 -0500

    Update stop_server.sh

--- stop_server.sh
+++ stop_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Splitting this command off allows the server to run in the background, and be killed by executing this script
 
-echo 1 > /var/www/gameservers/valheim/server_exit.drp
+v=`pidof valheim*`
+
+##Depreciated 02-09-21
+##echo 1 > /var/www/gameservers/valheim/server_exit.drp
+
+kill -2 $v
