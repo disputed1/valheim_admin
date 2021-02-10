a4e221249e46a2fc1210e9ff305c55900755d337
J.Mac <jmcnab1990@gmail.com>
2021-02-10 13:32:07 -0500

    Update start_server.sh
    updated script to run properly with crontab, added a cd in as the x86_64 had trouble executing

--- start_server.sh
+++ start_server.sh
@@ -5,9 +5,7 @@ export SteamAppId=892970
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-
-valheimserver=`./valheim_server.x86_64 -name "" -port 2456 -world "" -password "" >> /var/www/gameservers/backup/valheimserver.log &`
-
-$valheimserver
+cd /var/www/gameservers/valheim/
+./valheim_server.x86_64 -name "" -port 2456 -world "" -password "" >> /var/www/html/downloads/valheimserver.log &
 
 export LD_LIBRARY_PATH=$templdpath
