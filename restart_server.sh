11bcdf317b44ad9ff42d65059c9f4a54c7b34f30
J.Mac <jmcnab1990@gmail.com>
2021-02-10 12:07:35 -0500

    Update restart_server.sh
    added in map file backup and steamcmd update, also created a path "backup/" to place custom files and map file backups

--- restart_server.sh
+++ restart_server.sh
@@ -1,31 +1,49 @@
-v=`pidof valheim*`
-export v
-startscript=/path/to/file/valheim/start_server.sh
-log=/path/to/file/valheimserver.log
+#!/bin/bash
+
+## useful commands
+# ~/steamcmd +login anonymous +force_install_dir /var/www/gameservers/valheim/ +app_update 896660 validate +exit
+# echo 1 > /var/www/gameservers/valheim/server_exit.drp
+
+v=$(pidof valheim*)
+worldfiles=/home/steamuser/.config/unity3d/IronGate/Valheim/worlds
+startscript=/var/www/gameservers/valheim/start_server.sh
+log=/var/www/html/downloads/valheimserver.log
 timestamp=`date "+%H:%M:%S-%Y%m%d"`
 
 if [[ ! -z "$v"  ]];
   then
-    echo $timestamp "PID found, executing restart" $v >> $log
-    echo 1 > /path/to/file/valheim/server_exit.drp
+    echo $timestamp "PID found, executing restart" $v
+    kill -2 $v
     while [ -e /proc/$v ]
       do
         sleep .5
       done
-    echo $timestamp "server PID no longer exists" >> $log
-    $startscript
-    if [[ -z "$v"  ]];
-      v2=`pidof valheim*`
+    echo $timestamp "server PID no longer exists"
+    zip -r /var/www/gameservers/backup/$timestamp'_worldname'.zip /home/steamuser/.config/unity3d/IronGate/Valheim/worlds;
+    if [[ $? -eq 0 ]];
       then
-        echo $timestamp "server start failed, reattempting" >> $log
-        $startscript
-        wait $v
-        echo $timestamp "server start after restart completed" >> $log
+        echo $timestamp "world backup completed"
       else
-        echo $timestamp "server started successfully" >> $log
+    echo $timestamp "world file backup failed"
+    exit 1
+fi
+    /home/steamuser/.steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /var/www/gameservers/valheim/ +app_update 896660 validate +exit;
+    cp -pv /var/www/gameservers/backup/start_server.sh /var/www/gameservers/valheim/;
+    cp -pv /var/www/gameservers/backup/stop_server.sh /var/www/gameservers/valheim/;
+    cp -pv /var/www/gameservers/backup/restart_server.sh /var/www/gameservers/valheim/;
+    echo $timestamp "update completed / custom files copied"
+    $startscript
+    if [[ ! $? -eq 0  ]];
+      then
+        echo $timestamp "server start failed, reattempting"
+        $startscript
+        wait $?
+        echo $timestamp "server start after restart completed"
+      else
+        echo $timestamp "server started successfully"
     fi
  else
-    echo $timestamp "No server PID found, starting" >> $log
+    echo $timestamp "No server PID found, starting"
     $startscript
     wait $v
     echo $v
